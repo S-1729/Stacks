4b65b75d1ecc17b8dc9ea83b6223b0e63e8cbd89
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-07-12 18:19:45 +0530

    Add files via upload

---
+++ Maximum Score From Removing Substrings.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int n     = s.length();
+        int score = 0;
+        string maxStr = (x > y) ? "ab" : "ba";
+        string minStr = (x < y) ? "ab" : "ba";
+        string temp_first     = removeSubstring(s, maxStr);
+        int removedPairsCount = (n - temp_first.length()) / 2;
+        score                += removedPairsCount * max(x, y);
+        
+        string temp_second = removeSubstring(temp_first, minStr);
+        removedPairsCount  = (temp_first.length() - temp_second.length()) / 2;
+        score             += removedPairsCount * min(x, y);
+        return score;
+    }
+
+private:
+    string removeSubstring(string& s, string& matchStr) {
+        stack<char> st;
+        for (char &ch : s) {
+            if (ch == matchStr[1] && !st.empty() && st.top() == matchStr[0]) {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+
+        string remainStr;
+        while (!st.empty()) {
+            remainStr.push_back(st.top());
+            st.pop();
+        }
+        reverse(remainStr.begin(), remainStr.end());
+        return remainStr;
+    }
+};
+
+/*
+    You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+      .Remove substring "ab" and gain x points.
+        .For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+      .Remove substring "ba" and gain y points.
+        .For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+    Return the maximum points you can gain after applying the above operations on s.
+    
+    Example 1:
+    Input: s = "cdbcbbaaabab", x = 4, y = 5
+    Output: 19
+    Explanation:
+    - Remove the "ba" underlined in "cdbcbbaaabab". Now, s = "cdbcbbaaab" and 5 points are added to the score.
+    - Remove the "ab" underlined in "cdbcbbaaab". Now, s = "cdbcbbaa" and 4 points are added to the score.
+    - Remove the "ba" underlined in "cdbcbbaa". Now, s = "cdbcba" and 5 points are added to the score.
+    - Remove the "ba" underlined in "cdbcba". Now, s = "cdbc" and 5 points are added to the score.
+    Total score = 5 + 4 + 5 + 5 = 19.
+    
+    Example 2: 
+    Input: s = "aabbaaxybbaabb", x = 5, y = 4
+    Output: 20
+
+    T.C : O(n)
+    S.C : O(n)
+*/
