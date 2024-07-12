06fef8a27748645bf025cec4c333b57c476c3bba
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-07-12 18:21:43 +0530

    Add files via upload

--- Maximum Score From Removing Substrings.cpp
+++ Maximum Score From Removing Substrings.cpp
@@ -36,6 +36,48 @@ private:
     }
 };
 
+//Code-2
+//T.C : O(n)
+//S.C : O(1)
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int n     = s.length();
+        int score = 0;
+
+        string maxStr = (x > y) ? "ab" : "ba";
+        string minStr = (x < y) ? "ab" : "ba";
+        string temp_first     = removeSubstring(s, maxStr);
+        int L                 = temp_first.length();
+        int removedPairsCount = (n - L) / 2;
+        score                += removedPairsCount * max(x, y);
+
+        string temp_second = removeSubstring(temp_first, minStr);
+        removedPairsCount  = (L - temp_second.length()) / 2;
+        score             += removedPairsCount * min(x, y);
+
+        return score;
+    }
+
+    string removeSubstring(string& inputString, string& matchStr) {
+        int j = 0;
+
+        for (int i = 0; i < inputString.size(); i++) {
+            inputString[j++] = inputString[i];
+
+            if (j > 1 &&
+                inputString[j - 2] == matchStr[0] &&
+                inputString[j - 1] == matchStr[1]) {
+                j -= 2;
+            }
+        }
+
+        inputString.erase(inputString.begin() + j, inputString.end());
+
+        return inputString;
+    }
+};
+
 /*
     You are given a string s and two integers x and y. You can perform two types of operations any number of times.
       .Remove substring "ab" and gain x points.
